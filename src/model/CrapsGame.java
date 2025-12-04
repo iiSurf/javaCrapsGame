85fb24d713eb027ea94dfdd18abde92b8d1f0760
Nick Sandberg <nhumeniuk@go.shoreline.edu>
2025-12-03 23:31:46 -0800

    added dice roll and some game logic, carried over from yesterdays and todays work. Not currently runnable and no tests have been made yet.

--- src/model/CrapsGame.java
+++ src/model/CrapsGame.java
@@ -61,4 +61,64 @@ public class CrapsGame {
     public void removePropertyChangeListener(final PropertyChangeListener theListener) {
         myPcs.removePropertyChangeListener(theListener);
     }
+
+    private void endGame(final boolean thePlayerWon) {
+        int oldPlayerWins = myPlayerWins;
+        int oldHouseWins = myHouseWins;
+
+        myGameActive = false;
+        myGameWon = thePlayerWon;
+
+        if (thePlayerWon) {
+            myPlayerWins++;
+            myPcs.firePropertyChange("playerWins", oldPlayerWins, myPlayerWins);
+        } else {
+            myPlayerWins++;
+            myPcs.firePropertyChange("houseWins", oldHouseWins, myHouseWins);
+        }
+
+        myPcs.firePropertyChange("gameActive", true, false);
+        myPcs.firePropertyChange("gameWon", !thePlayerWon, thePlayerWon);
+    }
+
+    // TODO - check logic later on when testing
+    public void roll() {
+        if (!myGameActive) {
+            myGameActive = true;
+        }
+
+        final int myBothDiceSum = myDie1 + myDie2;
+        final int oldDie1 = myDie1;
+        final int oldDie2 = myDie2;
+        final int oldPoint = myPoint;
+        final int oldBothDieTotal = myBothDiceSum;
+
+        myDie1 = diceRoll();
+        myDie2 = diceRoll();
+        final int totalRoll = myBothDiceSum;
+
+        myPcs.firePropertyChange("die1", oldDie1, myDie1);
+        myPcs.firePropertyChange("die2", oldDie2, oldDie2);
+        myPcs.firePropertyChange("point", oldPoint, oldBothDieTotal);
+
+        if (myPoint == 0) {
+
+            if (oldBothDieTotal == 2 || oldBothDieTotal == 3 || oldBothDieTotal == 12) {
+                endGame(false);
+            } else if (oldBothDieTotal == 7 || oldBothDieTotal == 11) {
+                endGame(true);
+            } else {
+                myPoint = oldBothDieTotal;
+                myPcs.firePropertyChange("point", oldPoint, myPoint);
+            }
+        }
+        else {
+            if (oldBothDieTotal == myPoint) {
+                endGame(true);
+            }
+            else if (oldBothDieTotal == 7) {
+                endGame(false);
+            }
+        }
+    }
 }
